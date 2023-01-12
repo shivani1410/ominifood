3298dc25eb1033a7a4c530aea3bbade3e3d3a35e
Shivani <shivanimathur1410@gmail.com>
2023-01-12 15:04:26 +0530

    hero page and how to page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { HeroPageComponent } from './components/hero-page/hero-page.component';
+import { HeaderSectionComponent } from './components/header-section/header-section.component';
+import { HowSectionComponent } from './components/how-section/how-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    HeroPageComponent,
+    HeaderSectionComponent,
+    HowSectionComponent
   ],
   imports: [
     BrowserModule,
