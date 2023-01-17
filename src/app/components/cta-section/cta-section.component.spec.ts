c3095490b9017774c8cfaa21cd7242ad45ba76c7
Shivani <shivanimathur1410@gmail.com>
2023-01-17 16:37:53 +0530

    call to action section

---
+++ src/app/components/cta-section/cta-section.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CtaSectionComponent } from './cta-section.component';
+
+describe('CtaSectionComponent', () => {
+  let component: CtaSectionComponent;
+  let fixture: ComponentFixture<CtaSectionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CtaSectionComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CtaSectionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/cta-section/cta-section.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cta-section',
+  templateUrl: './cta-section.component.html',
+  styleUrls: ['./cta-section.component.css']
+})
+export class CtaSectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
